1ca49e811f0f19c9a31484123edc1a889eaa1d99
ccx1010sky <ccx1010@gmail.com>
2022-10-16 22:16:02 +0100

    set up redux store

--- store.js
+++ store.js
@@ -1,4 +1,4 @@
-import { ConfigureStore } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 import navReducer from "./slices/navSlice";
 
 export const store = configureStore({
