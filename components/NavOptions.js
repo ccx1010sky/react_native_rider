effd7b394d1c022c53117c8fbe6d00d0b3c26b20
ccx1010sky <ccx1010@gmail.com>
2022-10-17 12:40:38 +0100

    import TouchableOpacity from react-naive in NavOptions.js and setup data

--- components/NavOptions.js
+++ components/NavOptions.js
@@ -1,13 +1,33 @@
-import {  Text, View } from 'react-native'
-import React from 'react'
+import { FlatList, Text, View, TouchableOpacity } from "react-native";
+import React from "react";
+
+const data = [
+  {
+    id: "123",
+    title: "Get a ride",
+    image: "http://links.papareact.com/3pm",
+    screen: "MapScreen",
+  },
+  {
+    id: "456",
+    title: "Order food",
+    image: "https://links.papareact.com/28w",
+    screen: "EatsScreen",
+  },
+];
 
 const NavOptions = () => {
   return (
-    <View>
-      <Text>Hello World</Text>
-    </View>
-  )
-}
+    <FlatList
+      data={data}
+      horizontal
+      renderItem={({ item }) => (
+        <TouchableOpacity>
+          <Text>{item.title}</Text>
+        </TouchableOpacity>
+      )}
+    />
+  );
+};
 
-export default NavOptions
-
+export default NavOptions;
