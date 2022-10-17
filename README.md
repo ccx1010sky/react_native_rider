7268eeeabe3996cf92f5b84ebaa835b3b5488bde
ccx1010sky <ccx1010@gmail.com>
2022-10-17 20:15:59 +0100

    install UI tools

--- README.md
+++ README.md
@@ -1,5 +1,22 @@
+<!-- install expo tools -->
 npm -i -g expo-cli
-npx create-expo-app rider
+
+<!-- create a project -->
+npx create-expo-app rider 
+
+<!-- install Xcode simulator -->
+<!-- start the project server -->
+expo start (to start Metro Bundler)
+  press i to open iOS simulator
+  press r to reload app 
+
+<!-- install redux tools -->
 npm install @reduxjs/toolkit
 npm install react-redux (setup up redux store,store.js, navSlice.js)
+
+<!-- using tailwind CSS  -->
 npm install twrnc  (tailwind react native classnames)
+
+<!-- UI tools -->
+npm install react-native-elements (react UI tools)
+npm install react-native-vector-icons
--- components/NavOptions.js
+++ components/NavOptions.js
@@ -1,7 +1,8 @@
-import { FlatList, Text, View, TouchableOpacity,Image } from "react-native";
+import { FlatList, Text, View, TouchableOpacity, Image } from "react-native";
 import React from "react";
 import tw from "twrnc";
 
+// data input
 const data = [
   {
     id: "123",
@@ -22,10 +23,10 @@ const NavOptions = () => {
     <FlatList
       data={data}
       keyExtractor={(item) => item.id}
-        horizontal
+      horizontal
       renderItem={({ item }) => (
-        <TouchableOpacity style = {tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}>
-          <Text style = {tw`mt-2 text-lg font-semibold`}>{item.title}</Text>
+        <TouchableOpacity style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}>
+          <Text style={tw`mt-2 text-lg font-semibold`}>{item.title}</Text>
           <View>
             <Image
               style={{
